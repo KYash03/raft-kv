98ed4095465d1cf2292dd6908252b44d4c7c5e11
Yash Kumar <kyash03@student.ubc.ca>
2026-04-19 10:33:42 -0700

    fix gitignore + add cli

---
+++ cmd/raftkv/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"strconv"
+	"strings"
+	"syscall"
+
+	"github.com/KYash03/raft-kv/server"
+)
+
+func main() {
+	id := flag.Uint64("id", 0, "this node's id (must be > 0)")
+	addr := flag.String("addr", "", "listen address (passed to net.Listen)")
+	peerStr := flag.String("peers", "", "comma sep peer list of id@addr, include self")
+	flag.Parse()
+
+	if *id == 0 || *addr == "" || *peerStr == "" {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	peers, err := parsePeers(*peerStr)
+	if err != nil {
+		log.Fatalf("bad peers value, %v", err)
+	}
+
+	s := server.New(*id, *addr, peers)
+	if err := s.Start(); err != nil {
+		log.Fatalf("start, %v", err)
+	}
+	log.Printf("node %d listening on %s", *id, *addr)
+
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+	<-sig
+	log.Printf("shutting down")
+	s.Stop()
+}
+
+func parsePeers(s string) ([]server.Peer, error) {
+	var out []server.Peer
+	for _, p := range strings.Split(s, ",") {
+		p = strings.TrimSpace(p)
+		if p == "" {
+			continue
+		}
+		at := strings.IndexByte(p, '@')
+		if at < 0 {
+			return nil, fmt.Errorf("missing @ in %q", p)
+		}
+		id, err := strconv.ParseUint(p[:at], 10, 64)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, server.Peer{ID: id, Addr: p[at+1:]})
+	}
+	return out, nil
+}
